919929be0dfbf143d50c501b9145c6b6d4238794
Chris Cann <canniman@googlemail.com>
2021-04-11 20:54:10 +0100

    fix RangeSlider to not use IMGUI Layout functions

--- Assets/_Project/Scripts/Editor/Util/RangeSliderDrawer.cs
+++ Assets/_Project/Scripts/Editor/Util/RangeSliderDrawer.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,36 +7,90 @@ namespace Util
     [CustomPropertyDrawer(typeof(RangeSliderAttribute))]
     public class RangeSliderDrawer : PropertyDrawer
     {
+        // this needs to use GUI/EditorGUI calls rather than GUILayout/EditorGUILayout
+        // as the *Layout calls cause and exception in the editor
+        // apparently they are not supported for performance reasons
         public override void OnGUI(Rect position, SerializedProperty property,
             GUIContent label)
         {
-            var att = attribute as RangeSliderAttribute;
+            EditorGUI.BeginProperty(position, label, property);
+
             var minProperty = property.FindPropertyRelative("Start");
             var maxProperty = property.FindPropertyRelative("End");
 
 
-            using (new GUILayout.HorizontalScope())
-            {
-                GUILayout.Label(label, EditorStyles.boldLabel, GUILayout.Width(EditorGUIUtility.labelWidth));
-                float min = minProperty.floatValue, max = maxProperty.floatValue;
-                EditorGUILayout.MinMaxSlider( ref min, ref max, att.Min, att.Max);
-                minProperty.floatValue = min;
-                maxProperty.floatValue = max;
-            }
+            var labelRect = new Rect(
+                position.x, position.y, EditorGUIUtility.labelWidth, EditorGUIUtility.singleLineHeight
+            );
 
-            using (new GUILayout.HorizontalScope())
-            {
-                GUILayout.Label("", GUILayout.Width(EditorGUIUtility.labelWidth));
+            EditorGUI.LabelField(labelRect, label);
 
-                GUILayout.Label( "start", GUILayout.Width( 35 ) );
-                EditorGUILayout.PropertyField( minProperty, GUIContent.none, true, GUILayout.MinWidth( 30 ),GUILayout.MaxWidth( 75 ) );
+            var sliderRect = LayoutSlider(position, labelRect, minProperty, maxProperty);
 
-                GUILayout.FlexibleSpace();
+            var secondRowRect = sliderRect;
+            secondRowRect.y += EditorGUIUtility.singleLineHeight;
+            LayoutStartAndEnd(secondRowRect, minProperty, maxProperty);
 
-                GUILayout.Label( "end", GUILayout.Width( 30 ) );
-                EditorGUILayout.PropertyField( maxProperty, GUIContent.none, true, GUILayout.MinWidth( 30 ), GUILayout.MaxWidth( 75 ) );
 
-            }
+            EditorGUI.EndProperty();
+        }
+
+        private Rect LayoutSlider(Rect position, Rect labelRect, SerializedProperty minProperty,
+            SerializedProperty maxProperty)
+        {
+            var att = attribute as RangeSliderAttribute;
+
+            var sliderRect = new Rect(
+                labelRect.x + labelRect.width,
+                position.y,
+                position.width - labelRect.width,
+                EditorGUIUtility.singleLineHeight
+            );
+
+            float min = minProperty.floatValue;
+            float max = maxProperty.floatValue;
+
+            EditorGUI.MinMaxSlider(sliderRect, ref min, ref max, att.Min, att.Max);
+            minProperty.floatValue = min;
+            maxProperty.floatValue = max;
+            return sliderRect;
+        }
+
+        private void LayoutStartAndEnd(Rect overallRect, SerializedProperty minProperty,
+            SerializedProperty maxProperty)
+        {
+            var width = overallRect.width;
+            width -= 30 + 25; // for the labels;
+            width /= 2; // start and end
+
+            var fieldWidth = Mathf.Clamp(width, 58, 120);
+
+
+            var startLabelRect = overallRect;
+            startLabelRect.width = 30f;
+            GUI.Label(startLabelRect, "start");
+
+            var startFieldRect = startLabelRect;
+            startFieldRect.x += 20f;
+            startFieldRect.width = fieldWidth;
+            EditorGUI.PropertyField(startFieldRect, minProperty, GUIContent.none, true);
+
+
+            var endFieldRect = overallRect;
+            endFieldRect.x = overallRect.xMax - fieldWidth;
+            endFieldRect.width = fieldWidth;
+            EditorGUI.PropertyField(endFieldRect, maxProperty, GUIContent.none, true);
+
+            var endLabelRect = endFieldRect;
+            endLabelRect.x = endFieldRect.x - 15f;
+            endLabelRect.width = 25f;
+            GUI.Label(endLabelRect, "end");
+        }
+
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight * 2;
         }
     }
 }
