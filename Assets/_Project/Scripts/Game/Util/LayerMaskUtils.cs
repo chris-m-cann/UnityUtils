ab799e7fb9c6db8b3787691d1d80ae216f56e967
Chris Cann <canniman@googlemail.com>
2021-04-15 18:55:11 +0100

    LayerUtils

---
+++ Assets/_Project/Scripts/Game/Util/LayerMaskUtils.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Util
+{
+    public static class LayerMaskUtils
+    {
+        public static bool Contains(this LayerMask self, int layer)
+        {
+            var layerBit = 1 << layer;
+
+            return (self.value & layerBit) == layerBit;
+        }
+
+
+        public static int FirstLayerSet(this LayerMask self)
+        {
+            if (self.value == 0) return 0;
+
+            int layer = 0;
+            while ((self.value & (1 << layer)) == 0)
+            {
+                ++layer;
+            }
+
+            return layer;
+        }
+    }
+}
